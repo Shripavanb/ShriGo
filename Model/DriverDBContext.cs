58120c08ded917578218cf41f5bfd49c86344574
ShriGit01 <shripavanb@gmail.com>
2026-04-12 11:54:01 +0530

    Reapply "Twilio for mobile verification,Drivers table, rideinput update, Signinand sigup update"
    This reverts commit d45f60cb129f6d8f10652fbf06804b0279906670.

---
+++ Model/DriverDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShriGo.Model
+{
+    public class DriverDBContext: DbContext
+    {
+        public DriverDBContext(DbContextOptions<DriverDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<DriverModel> DriversTb { get; set; }
+
+
+    }
+}
