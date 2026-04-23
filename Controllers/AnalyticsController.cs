b6e87574644f09c3bd606297d5c4267fc33784f2
ShriGit01 <shripavanb@gmail.com>
2026-04-23 19:09:40 +0530

    Test code which works both locally and on server app

---
+++ Controllers/AnalyticsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AnalyticsController : ControllerBase
+{
+    [HttpGet("test")]
+    public IActionResult Test()
+    {
+        return Ok("API working");
+    }
+}
