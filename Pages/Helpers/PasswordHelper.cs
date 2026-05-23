d5d45a3044f3f4be47cbefab10c6b3991232813d
shrifoodsGit <shrifoodspb@gmail.com>
2026-05-23 19:15:18 +0530

    Changes done as per Git Gaurdian doesnt allow

--- Pages/Helpers/PasswordHelper.cs
+++ Pages/Helpers/PasswordHelper.cs
@@ -29,7 +29,8 @@ namespace ShriGo.Pages.Helpers
 
         public bool IsHashed(string password)
         {
-            return password.StartsWith("AQAAAA");
+            return !string.IsNullOrWhiteSpace(password)
+                   && password.Length > 50;
         }
     }
 }
