b83a6a06440bb5bb8dd4c493f1587aee5527f85e
shrifoodsGit <shrifoodspb@gmail.com>
2026-05-29 20:00:11 +0530

    Updating for Active and Expired rides(History)

--- Controllers/RideApiController.cs
+++ Controllers/RideApiController.cs
@@ -15,14 +15,79 @@ namespace ShriGo.Controllers
             _dbContext = context;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> GetRides()
+        [HttpGet("active")]
+        public async Task<IActionResult> GetActiveRides()
         {
+            var now =
+                DateTime.Now;
+
             var rides = await _dbContext
                 .Ride_DBTable
+                .Where(r =>
+
+                    r.RideDate != null &&
+                    r.RideTime != null &&
+
+                    r.RideDate
+                        .Value
+                        .ToDateTime(
+
+                            r.RideTime.Value
+                        )
+                        .AddHours(2)
+
+                        >= now
+                )
+                .OrderBy(r => r.RideDate)
+                .ThenBy(r => r.RideTime)
                 .ToListAsync();
 
             return Ok(rides);
+        }
+
+
+        [HttpGet("history")]
+        public async Task<IActionResult> GetRideHistory()
+        {
+            var now =
+                DateTime.Now;
+
+            var rides = await _dbContext
+                .Ride_DBTable
+                .Where(r =>
+
+                    r.RideDate != null &&
+                    r.RideTime != null &&
+
+                    r.RideDate
+                        .Value
+                        .ToDateTime(
+
+                            r.RideTime.Value
+                        )
+                        .AddHours(2)
+
+                        < now
+                )
+                .OrderByDescending(r => r.RideDate)
+                .ThenByDescending(r => r.RideTime)
+                .ToListAsync();
+
+            return Ok(rides);
+        }
+        [HttpGet("Expired")]
+        public async Task<IActionResult> GetExpiredRides()
+        {
+            var today = DateOnly.FromDateTime(DateTime.Today);
+
+            var oldRides = await _dbContext
+                .Ride_DBTable
+                .Where(r => r.RideDate < today)
+                .OrderByDescending(r => r.RideDate)
+                .ThenByDescending(r => r.RideTime)
+                .ToListAsync();
+
+            return Ok(oldRides);
         }
     }
 }
