6504df4609f28030e5c3e7cd884e37fba3a8c043
shrifoodsGit <shrifoodspb@gmail.com>
2026-07-01 14:02:03 +0530

    Implemented VersionApiController and VersionModel for app version management

---
+++ Controllers/VersionApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShriGo.Model;
+
+namespace ShriGo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VersionApiController : ControllerBase
+    {
+        [HttpGet("Latest")]
+        public IActionResult GetLatestVersion()
+        {
+            VersionModel version = new VersionModel
+            {
+                LatestVersion = "20.3.0",
+                MinimumVersion = "20.2.9",
+                ForceUpdate = false,
+                Message = "A new version of ShriGo is available.",
+                PlayStoreUrl = "https://play.google.com/store/apps/details?id=in.shrigo.app"
+            };
+
+            return Ok(version);
+        }
+    }
+}
---
+++ Model/VersionModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ShriGo.Model
+{
+    public class VersionModel
+    {
+        public string LatestVersion { get; set; }
+
+        public string MinimumVersion { get; set; }
+
+        public bool ForceUpdate { get; set; }
+
+        public string Message { get; set; }
+
+        public string PlayStoreUrl { get; set; }
+    }
+}
