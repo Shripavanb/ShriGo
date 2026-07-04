fddc89174cd2c0b7f2a101397f52c43de066a3fc
shrifoodsGit <shrifoodspb@gmail.com>
2026-07-04 18:48:51 +0530

    Making DYnamic Version API

--- Controllers/VersionApiController.cs
+++ Controllers/VersionApiController.cs
@@ -7,19 +7,15 @@ namespace ShriGo.Controllers
     [Route("api/[controller]")]
     public class VersionApiController : ControllerBase
     {
-        [HttpGet("Latest")]
-        public IActionResult GetLatestVersion()
-        {
-            VersionModel version = new VersionModel
-            {
-                LatestVersion = "20.3.0",
-                MinimumVersion = "20.2.9",
-                ForceUpdate = false,
-                Message = "A new version of ShriGo is available.",
-                PlayStoreUrl = "https://play.google.com/store/apps/details?id=in.shrigo.app"
-            };
+     [HttpGet("Latest")]
+		public IActionResult GetLatestVersion()
+		{
+			var version = _context.VersionTb.FirstOrDefault();
 
-            return Ok(version);
-        }
+			if (version == null)
+				return NotFound();
+
+			return Ok(version);
+		}
     }
 }
